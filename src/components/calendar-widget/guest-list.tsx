4dda47a1f43bbffd22c4dacf8606fc3484efa0c6
prosper <jajaprosper70@gmail.com>
2025-03-05 12:22:01 +0100

    border color addition

--- src/components/calendar-widget/guest-list.tsx
+++ src/components/calendar-widget/guest-list.tsx
@@ -55,7 +55,7 @@ export const GuestList = () => {
                     : idx !== 0
                     ? "-ml-[.75rem]"
                     : null,
-                  selected === guest?.id ? "border-violet-10" : null,
+                  selected === guest?.id ? "border-[#7839ee]" : null,
                   "border-[2.5px] border-white rounded-full !size-[2rem] transition-all duration-200"
                 )}
               />
